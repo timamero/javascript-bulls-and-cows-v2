031a595fda5df47c0b5f12a99e3ba0a046b443be
Fatima Camero <fcamero12@gmail.com>
2021-11-25 11:03:19 -0800

    toggle instructions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 import 'beercss'
 
+/* Menu Elements */
 const menu = document.getElementById('menu')
 const menuBtn = document.getElementById('menu-btn')
 const hideMenuBtn = document.getElementById('hide-menu-btn')
+const instructionsBtn = document.getElementById('instructions-btn')
+/* Menu View Elements */
+const instructions = document.getElementById('instructions')
 
+
+/* User Events - Menu */
 menuBtn.addEventListener('click', () => {
-  console.log('clicked')
   menu.classList.toggle('hide')
+
+  // If instructions are visible and user clicks the home buttons, instructions will be made hidden
+  if (!instructions.classList.contains('hide')) {
+    instructions.classList.toggle('hide')
+  }
 })
 
 hideMenuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hide')
+})
+
+instructionsBtn.addEventListener('click', () => {
+  console.log('clicked')
+  instructions.classList.toggle('hide')
   menu.classList.toggle('hide')
 })
