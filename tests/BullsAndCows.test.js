f139ac168e9f46dde36d04298862fd4b386b6e39
Fatima Camero <fcamero12@gmail.com>
2021-12-02 21:19:25 -0800

    add tests for validation

--- tests/BullsAndCows.test.js
+++ tests/BullsAndCows.test.js
@@ -4,6 +4,9 @@ test.only('guess validation method checks for four digit number', () => {
   const guessArr = [
     ['1234', true],
     ['0000', true],
+    ['0404', true],
+    ['0005', true],
+    ['000000', false],
     ['abcd', false],
     ['a1b2', false],
     ['12345', false],
